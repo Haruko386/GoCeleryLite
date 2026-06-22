f09801b46bab1980d59537398e79dff53da90e67
Haruko386 <tryeverypossible@163.com>
2026-06-23 07:50:28 +0800

    init: new feat

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,1 +1,28 @@
-package server
+package router
+
+import (
+	"GoCeleryLite/internal/server"
+	"github.com/gin-gonic/gin"
+)
+
+type Router struct {
+	httpHandler server.Server
+}
+
+func (r *Router) Setup(engine *gin.Engine) {
+	engine.Use(func(c *gin.Context) {
+		c.Header("X-API-Source", "go")
+		c.Next()
+	})
+
+	v1 := engine.Group("/api/v1")
+	{
+		v1.POST("/tasks")       // Submit task
+		v1.GET("/tasks")        // Get tasks
+		v1.GET("/tasks/:id")    // Get task detail by ID
+		v1.DELETE("/tasks/:id") // Cancel task by id
+
+		v1.GET("/stats")  // Get queue stats
+		v1.GET("/health") // Check queue health
+	}
+}
